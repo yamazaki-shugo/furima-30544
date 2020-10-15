b642f458f9326c647828c41232b3e154afa04e26
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-15 17:44:47 +0900

    usersモデル単体テストコード(正常系)

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname  {Faker::Name.initials(number: 2)}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name_zen {"山田"}
+    last_name_zen {"三郎"}
+    first_name_kana {"ヤマダ"}
+    last_name_kana {"サブロウ"}
+    birth_day {1955-03-03}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nickname, email, password, first_name_zen, last_name_zen, first_name_kana, last_name_kana, birth_dayが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上で英字と数字の両方を含んでいれば登録できる" do
+        @user.password = "s12345"
+        @user.password_confirmation = "s12345"
+        expect(@user).to be_valid
+      end
+      it "first_name_zen, last_name_zenが全角かなであれば登録できる" do
+        @user.first_name_zen = "山田"
+        @user.last_name_zen = "四郎"
+        expect(@user).to be_valid
+      end
+      it "first_name_kana, last_name_kanaが全角カタカナであれば登録できる" do
+        @user.first_name_kana = "ヤマダ"
+        @user.last_name_kana = "シロウ"
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
