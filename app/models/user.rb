37067d83407b33f468cc347392c5a18badc511e4
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-15 12:18:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do 
+    validates :nickname
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :first_name_zen
+      validates :last_name_zen
+    end
+    with_options format: { with: /\A[ァ-ン]+\z/ }do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    validates :birth_day
+  end
+  
 end
