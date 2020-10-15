53e8b65625da86ebe5f56b0ecbf1a5868ca3f045
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-15 19:40:37 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,17 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX }
 
-  with_options presence: true do 
+  with_options presence: true do
     validates :nickname
 
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
       validates :first_name_zen
       validates :last_name_zen
     end
-    with_options format: { with: /\A[ァ-ン]+\z/ }do
+    with_options format: { with: /\A[ァ-ン]+\z/ } do
       validates :first_name_kana
       validates :last_name_kana
     end
     validates :birth_day
   end
-  
 end
