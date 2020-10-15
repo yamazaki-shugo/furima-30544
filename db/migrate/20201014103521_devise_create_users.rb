d2b006f6b6b5820934df685813aae95d0001291d
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-15 09:34:43 +0900

    usersテーブル作成

--- db/migrate/20201014103521_devise_create_users.rb
+++ db/migrate/20201014103521_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname
+      t.string :first_name_zen
+      t.string :last_name_zen
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.date :birth_day
 
       ## Recoverable
       t.string   :reset_password_token
