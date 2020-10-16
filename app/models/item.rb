1a46cad02128fb6b5afd924bf7180c9672e0932b
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-16 15:35:55 +0900

    各ActiveHashモデル(itemテーブル)作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :burden_type
+  belongs_to_active_hash :category
+  belongs_to_active_hash :days_to_ship
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :prefecture
 end
