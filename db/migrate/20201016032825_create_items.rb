fb35db67d197428a1953f54d3b4c6c2911596ee4
yamazaki-shugo <yamaz1209876@icloud.com>
2020-10-16 12:42:15 +0900

    itemsテーブル作成

--- db/migrate/20201016032825_create_items.rb
+++ db/migrate/20201016032825_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
+      t.string :name
+      t.text :description
+      t.integer :category_id
+      t.integer :item_status_id
+      t.integer :burden_type_id
+      t.integer :shipping_area_id
+      t.integer :days_to_ship_id
+      t.integer :price
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_103521) do
+ActiveRecord::Schema.define(version: 2020_10_16_032825) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "category_id"
+    t.integer "item_status_id"
+    t.integer "burden_type_id"
+    t.integer "shipping_area_id"
+    t.integer "days_to_ship_id"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_10_14_103521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
